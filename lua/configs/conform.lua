a4008487844d76ee0e699f1915bae3c05f67ff7d
Lasse Herzog <lasse@herzoeglich.de>
2025-09-21 23:52:51 +0200

    Update conform.lua

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,20 @@
 local options = {
-  formatters_by_ft = {
+  formatters_by_ft = {  
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    html = { "prettierd" },
+    javascript = { "prettierd" },
+    nix = { "alejandra" },
+    python = { "isort", "black" },
+    rust = { "rustfmt" },
+    svelte = { "prettierd" },
+    typescript = { "prettierd" },
+    typescriptreact = { "prettierd" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
